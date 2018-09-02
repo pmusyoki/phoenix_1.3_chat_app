34d8155482926546e17a99ff44b53acd4be21d39
Philip Musyoki <pmusyok@ncsu.edu>
2018-09-02 12:55:20 -0400

    Deploy to Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :chatter, ChatWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "wolfchat.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: "wolf-chat.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
@@ -13,5 +13,3 @@ config :chatter, Chatter.Repo,
   ssl: true
 
 config :logger, level: :info
-
-import_config "prod.secret.exs"
